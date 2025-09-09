247d670cfd9dfdb76a2e5c381c0cc4f1906e0bc0
Shaurya Sagar <shauryasagar@proton.me>
2025-09-09 13:09:58 +0530

    API Vulnerability fixed

--- script.js
+++ script.js
@@ -12,39 +12,33 @@ setTimeout(function () {
     document.getElementById('nyan').style.display = 'block';
 }, 150000);
 
-var lastfmData = {
-    baseURL: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=",
-    user: "itzzkobra",
-    api_key: "99d2c48aa9dc8a960f85a1765e11bb80",
-    additional: "&format=json&limit=1"
-};
 
 var getSetLastFM = function () {
     jQuery.ajax({
         type: "GET",
-        url: lastfmData.baseURL + lastfmData.user + "&api_key=" + lastfmData.api_key + lastfmData.additional,
+        url: "https://api.itsureya.com/lastfm",   // Updated to call internal API
         dataType: "json",
         success: function (resp) {
             var recentTrack = resp.recenttracks.track[0];
-            var formatted = recentTrack.name.replace(/\(.*\)|\[.*\]/g, '');;
+            var formatted = recentTrack.name.replace(/\(.*\)|\[.*\]/g, '');
 
             jQuery("span#tracktitle")
-                .html(formatted)
-                .attr("href", recentTrack.url)
-                .attr("title", recentTrack.name + " by " + recentTrack.artist["#text"])
-                .attr("target", "_blank");
+            .html(formatted)
+            .attr("href", recentTrack.url)
+            .attr("title", recentTrack.name + " by " + recentTrack.artist["#text"])
+            .attr("target", "_blank");
 
             jQuery("a#tothelink")
-                .attr("href", recentTrack.url)
+            .attr("href", recentTrack.url);
 
             var artistFormatted = recentTrack.artist["#text"];
             jQuery("span#trackartist")
-                .html(artistFormatted)
-                .attr("title", "Artist : " + recentTrack.artist["#text"]);
+            .html(artistFormatted)
+            .attr("title", "Artist : " + recentTrack.artist["#text"]);
 
             $("img#trackart").attr("src", recentTrack.image[2]["#text"]);
         },
-        error: function (resp) {
+        error: function () {
             jQuery("span#tracktitle").html("The sounds of silence");
             jQuery("img#trackart").attr("src", "https://i.imgur.com/Q6cCswP.jpg");
             var artistFormatted = "<img src='https://i.imgur.com/fae5XZA.png'>of silence";
@@ -52,6 +46,7 @@ var getSetLastFM = function () {
         }
     });
 };
+
 getSetLastFM();
 setInterval(getSetLastFM, 30 * 1000);
 
@@ -82,7 +77,7 @@ function changeBackground() {
         "https://i.postimg.cc/3xPsKqK5/bg3.webp",
         "https://i.postimg.cc/pTv3jnhs/bg4.webp",
         "https://i.postimg.cc/JnQv54jM/bg5.webp",
-     // "https://i.postimg.cc/nV0NsrH1/bg6.webp",
+        // "https://i.postimg.cc/nV0NsrH1/bg6.webp",
         "https://i.postimg.cc/vTqKCqPK/bg7.webp",
         "https://i.postimg.cc/65qgCKqx/bg8.webp",
         "https://i.postimg.cc/5NrG3rQY/bg9.webp",
